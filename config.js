f726257c56d593afeeeac773266708820291c8a9
AZ <alex.hc.zhang@gmail.com>
2025-10-28 23:04:18 -0700

    Import from VS Code

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js, to store the API keys
+
+export const OPENAI_API_KEY =
+  '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';
+export const GOOGLE_TRANSLATION_API_KEY =
+  'AIzaSyCQxmgQFyB9gsxIVGFvtsGYSNKjG5nF-IE';
